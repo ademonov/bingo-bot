9e2d53e4c49b1338e51f0bb86690540bc69c6b79
Andrew Demonov <fcoder@live.com>
2016-12-19 00:12:12 -0500

    GitterMdFormatProvider

--- src/formatting/mod.rs
+++ src/formatting/mod.rs
@@ -6,5 +6,10 @@ pub trait Format {
     fn format(&self, fp: &FormatProvider) -> String;
 }
 
+pub struct GitterMdFormatProvider;
 
-
+impl FormatProvider for GitterMdFormatProvider {
+     fn get_strikethrough(&self, s: &str) -> String {
+         format!("~~{}~~", s)
+     }
+}
