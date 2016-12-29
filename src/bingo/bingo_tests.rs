13b118756510a8c0c67225f8a35ed317dc0e16e2
Andrew Demonov <fcoder@live.com>
2016-12-29 01:04:50 -0500

    tests for Card, cleanup

--- src/bingo/bingo_tests.rs
+++ src/bingo/bingo_tests.rs
@@ -25,6 +25,10 @@ fn cardcell_check() {
 }
 
 #[test]
-fn card_new() {
-    let c = cards::Card::new();
+fn card_check_cell() {
+    let words = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"];
+    let mut c = cards::Card::new(words.iter());
+
+    c.check_cell(0, 0);
+    c.check_cell(cards::ROW_COUNT - 1, cards::COL_COUNT - 1);
 }
--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,12 @@ mod http;
 mod formatting;
 mod bingo;
 
-use formatting::*;
-
 fn main() {
     logger::init();
     info!("Starting...");
 
-    let words = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"];
-    print_iter(words.iter());
+    let format_provider = formatting::GitterMdFormatProvider::new();
 
-    let fp = formatting::GitterMdFormatProvider::new();
-
-    let mut c = bingo::cards::Card::new(words.iter());
-    c.check_cell(1,2);
-    let sc = c.format(&fp);
-    println!("{}", sc);
-
-    //http::listening("0.0.0.0:8181");
-    info!("Done.")
+    http::listening("0.0.0.0:8181");
+    info!("Done.");
 } 
-
-fn print_iter<I, S>(iterator: I) where I: Iterator<Item=S>, S: AsRef<str> {
-    for item in iterator {
-        print!("{} ", item.as_ref())
-    }
-    println!();
-}
