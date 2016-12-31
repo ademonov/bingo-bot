b33732b68bba6872b1046f9f56a633e7df9af0a3
Andrew Demonov <fcoder@live.com>
2016-12-31 02:09:46 -0500

    some routing

--- src/http.rs
+++ src/http.rs
@@ -1,13 +1,24 @@
-use hyper::server::{Server, Request, Response};
-//use hyper::server::Listening;
+use hyper::method::Method;
+use hyper::server::{Server, Request, Response, Listening};
+use hyper::status::StatusCode;
+use hyper::uri::RequestUri;
 
 pub fn listening(url: &str) {
    let listener = Server::http(&url).unwrap();
    listener.handle(handle_proc).unwrap();
 }
 
-fn handle_proc(_: Request, res: Response) {
-    let s_buf = String::from("Helo, World!");
+fn handle_proc(req: Request, res: Response) {
+    let mut s_buf:String;
+    match (req.method, req.uri) {
+        (Method::Get, RequestUri::AbsolutePath(ref s)) => {
+            s_buf = format!("{}", s.to_owned());
+        }
+        (_, _) => {
+            s_buf = String::from("404 Not found!");
+            res.status_mut = StatusCode::NotFound;
+        }
+    }
     let buf = s_buf.as_bytes();
     res.send(buf).unwrap();
 }
